960305ca7852927fd103d806072a26b6e7f023e0
Gabe Kessler <gabe@impdesigns.com>
2011-09-22 22:52:29 -0400

    refactored module

--- lib/awesome_flags.rb
+++ lib/awesome_flags.rb
@@ -1,17 +1,16 @@
 module AwesomeFlags
-    
+      
   def all_flags(column = nil)
-    a = self.flag_mapping
-    if column.nil?
-      c = a.values.map {|var| var.keys}.flatten
-    else
-      b = a[column]
-      c = Array.[](b.keys).flatten
-    end
+    c = get_array_of_flags(column)
     c.map
   end
   
   def my_flags(column = nil)
+    c = get_array_of_flags(column)
+    c.map {|var| self.send(var) ? var : nil}.compact!
+  end
+  
+  def get_array_of_flags(column)
     a = self.flag_mapping
     if column.nil?
       c = a.values.map {|var| var.keys}.flatten
@@ -19,7 +18,6 @@ module AwesomeFlags
       b = a[column]
       c = Array.[](b.keys).flatten
     end
-    c.map {|var| self.send(var) ? var : nil}.compact!
   end
   
 end
